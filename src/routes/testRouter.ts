0ca58a9c5db5428671868ab34a68c267dd87134c
luiz <lguilhermefl@gmail.com>
2022-09-16 17:23:44 -0300

    feat: add test creation router

---
+++ src/routes/testRouter.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import validateBodySchemaMiddleware from "../middlewares/validateBodySchema";
+import validateJWT from "../middlewares/validateJwtMiddleware";
+import { testSchema } from "../schemas/testSchema";
+import * as testController from "../controllers/testController";
+
+const testRouter = Router();
+
+testRouter.use(validateJWT());
+testRouter.post(
+  "/tests",
+  validateBodySchemaMiddleware(testSchema),
+  testController.insert
+);
+
+export default testRouter;
